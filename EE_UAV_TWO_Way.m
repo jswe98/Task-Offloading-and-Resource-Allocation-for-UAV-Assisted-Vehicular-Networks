dbf885bc17d4be466244e27de83259d76bbf047b
jswe98 <jswe98@icloud.com>
2023-07-05 10:21:22 +0800

    add bor code

--- EE_UAV_TWO_Way.m
+++ EE_UAV_TWO_Way.m
@@ -1,8 +1,17 @@
 clc
 close all
 clear
+T=70;%共有70个时隙
+t=1;%第一个时隙
 lleight=1;%车道索引车向右
 lleft=-1;%车道索引车向左
+%UAV coordinate setup
+VUAV_max=10;%无人机速度约束
+VUAU_x
+VUAU_y
+for t=1:T
+QUAV(t)=[VUAV_max*t,VUAV_max*t,100];
+end
 %vehicle coordinate setup
 for i=1:4
     x_user(i)=6;
