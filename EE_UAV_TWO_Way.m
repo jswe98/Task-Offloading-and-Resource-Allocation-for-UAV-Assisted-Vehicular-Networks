6ea3d961feadf301a1c048575a9334bf12a03c4a
jswe98 <jswe98@icloud.com>
2023-07-05 09:59:25 +0800

    add yue code

--- EE_UAV_TWO_Way.m
+++ EE_UAV_TWO_Way.m
@@ -1,1 +1,16 @@
+clc
+close all
+clear
+lleight=1;%车道索引车向右
+lleft=-1;%车道索引车向左
 %vehicle coordinate setup
+for i=1:4
+    x_user(i)=6;
+    y_user(i)=2;
+end
+x_user(1)=6;
+x_user(2)=8;
+x_user(3)=8;
+y_user(1)=2;
+y_user(2)=1.5;
+y_user(3)=2;
